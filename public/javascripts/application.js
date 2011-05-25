a138edc428162ed2b0335859027721c2d54e595d
Brian Chamberlain <b-chamberlain@northwestern.edu>
2011-05-25 18:43:34 -0500

    added example other field enable or disable to management tasks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,3 +13,32 @@ function check_for_destroy(element) {
 	}
 }
 
+// Used inside document ready method call to wire up selects with other fields
+function wire_up_select_other(select_id, other_id){
+  check_select_for_other(select_id, other_id);
+  // Check on item change
+  $(select_id).change(function(){
+    check_select_for_other(select_id, other_id);
+  });
+}
+
+// Used to enable/disable 'other' input type text field
+function check_select_for_other(select_id, other_id){
+  var s = $(select_id+" option:selected");
+  var o = $(other_id);
+
+  // making sure the object id's given above exist on page
+  if((o.size() > 0) && (s.size() > 0)){
+    // 'Other' = -5
+    if(s.val() == "-5"){
+        o.removeAttr('disabled');
+        o.css('background-color', '#EEF1C3'); //to make the change more visible
+    }
+    else{
+        o.val(''); //clear the other field
+        o.attr('disabled', 'disabled');
+        o.css('background-color', '#d0d0d0'); // to make disabled more visible
+    }
+  }
+}
+
