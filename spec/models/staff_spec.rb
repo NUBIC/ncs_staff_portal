3c6e6ef123091f5addb0b6295855d2e9aa651d0c
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-06-03 14:19:02 -0500

    Added validations for staff hourly rate.

--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Staff do
-  pending
+  before(:each) do
+    @staff = Staff.create(:name => "Testing", 
+                 :netid => "test123",
+                 :email => "test@test.com",
+                 :study_center => 123456)
+  end
+  describe "hourly_rate validations" do
+    it "should not contain other than decimal value" do
+      @staff.hourly_rate = "test"
+      @staff.should_not be_valid
+      @staff.should have(1).error_on(:hourly_rate)
+    end
+    it "should be greater than 0 dollar" do
+      @staff.hourly_rate = -3
+      @staff.should_not be_valid
+      @staff.should have(1).error_on(:hourly_rate)
+    end
+  end
 end
