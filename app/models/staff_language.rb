07007a341659ab3cc895f6276a1088cbef419ca6
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-06-15 10:20:22 -0500

    Correct format for validation.

--- app/models/staff_language.rb
+++ app/models/staff_language.rb
@@ -1,5 +1,5 @@
 class StaffLanguage < ActiveRecord::Base
   belongs_to :staff
-  validates_presence_of:lang_code
+  validates_presence_of :lang_code
   belongs_to :lang, :conditions => "list_name = 'LANGUAGE_CL2'", :class_name => 'NcsCode', :primary_key => :local_code, :foreign_key => :lang_code
 end
