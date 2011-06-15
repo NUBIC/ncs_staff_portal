4dd274d5e7b33a82a214a3fe6bd10a1839c336c8
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-06-15 10:19:39 -0500

    Removed unnecessary code.

--- app/models/outreach_race.rb
+++ app/models/outreach_race.rb
@@ -1,5 +1,4 @@
 class OutreachRace < ActiveRecord::Base
   belongs_to :outreach_event
-  attr_accessor :should_destroy
   belongs_to :race, :conditions => "list_name = 'RACE_CL3'", :class_name => 'NcsCode', :primary_key => :local_code, :foreign_key => :race_code
 end
