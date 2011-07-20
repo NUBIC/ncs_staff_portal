27fb03e941a181d035599f1138a0b887ac095b73
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-07-20 10:53:15 -0500

    Updates the aker configuration.

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,18 @@ module OMA
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    Aker.configure do
+      # The authentication protocol to use for interactive access.
+      # `:form` is the default.
+      ui_mode :cas
+
+      # The authentication protocol(s) to use for non-interactive
+      # access.  There is no default.
+      # api_mode :http_basic
+
+      # The portal to which this application belongs.  Optional.
+      portal :StaffPortal
+    end
   end
 end
