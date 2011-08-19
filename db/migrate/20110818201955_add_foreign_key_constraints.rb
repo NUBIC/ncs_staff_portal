cb4d58298f291846ef35580db17d56a6c723a25f
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-08-18 22:09:57 -0500

    Temporarily disable foreign key constraint for management_tasks for production server deploy.

--- db/migrate/20110818201955_add_foreign_key_constraints.rb
+++ db/migrate/20110818201955_add_foreign_key_constraints.rb
@@ -4,7 +4,7 @@ class AddForeignKeyConstraints < ActiveRecord::Migration
     execute "ALTER TABLE staff_languages ADD CONSTRAINT fk_staff_languages_staff FOREIGN KEY (staff_id) REFERENCES staff(id)"
     execute "ALTER TABLE staff_weekly_expenses ADD CONSTRAINT fk_staff_weekly_expenses_staff FOREIGN KEY (staff_id) REFERENCES staff(id)"
     
-    execute "ALTER TABLE management_tasks ADD CONSTRAINT fk_management_tasks_weekly_expense FOREIGN KEY (staff_weekly_expense_id) REFERENCES staff_weekly_expenses(id)"
+    # execute "ALTER TABLE management_tasks ADD CONSTRAINT fk_management_tasks_weekly_expense FOREIGN KEY (staff_weekly_expense_id) REFERENCES staff_weekly_expenses(id)"
     
     execute "ALTER TABLE outreach_races ADD CONSTRAINT fk_outreach_races_event FOREIGN KEY (outreach_event_id) REFERENCES outreach_events(id)"
     execute "ALTER TABLE outreach_targets ADD CONSTRAINT fk_outreach_targets_event FOREIGN KEY (outreach_event_id) REFERENCES outreach_events(id)"
@@ -28,7 +28,7 @@ class AddForeignKeyConstraints < ActiveRecord::Migration
     execute "ALTER TABLE staff_languages DROP CONSTRAINT fk_staff_languages_staff"
     execute "ALTER TABLE staff_weekly_expenses DROP CONSTRAINT fk_staff_weekly_expenses_staff"
     
-    execute "ALTER TABLE management_tasks DROP CONSTRAINT fk_management_tasks_weekly_expense"
+    # execute "ALTER TABLE management_tasks DROP CONSTRAINT fk_management_tasks_weekly_expense"
     
     execute "ALTER TABLE outreach_races DROP CONSTRAINT fk_outreach_races_event" 
     execute "ALTER TABLE outreach_targets DROP CONSTRAINT fk_outreach_targets_event" 
