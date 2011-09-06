75edc96c99bdc6daefaacfacb464f24122fb4a0e
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-09-06 12:55:36 -0500

    Use the rake task for creating tag and versioning the application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,7 +60,13 @@ module ApplicationHelper
   end
 
   def app_version_helper
-    "Release Version #{ApplicationController::APP_VERSION}"
+    version_filename = "#{Rails.root}/config/app_version.yml"
+    version  = "0.0.0"
+    if File.exists?(version_filename)
+      app_version = YAML.load_file(version_filename)
+      version = "#{app_version["major"]}.#{app_version["minor"]}.#{app_version["revision"]}"
+    end
+    "Release Version #{version}"
   end
   
   class NCSTabsBuilder < TabsOnRails::Tabs::TabsBuilder
