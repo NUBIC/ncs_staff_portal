2b5ed1166b4ab4333b43bd9ea22a9006466e9863
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-09-15 11:31:10 -0500

    Another fix for CI build by loading configuration file in spec_helper. #1560.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-
+require 'ncs_navigator/configuration'
+NcsNavigator.configuration =
+  NcsNavigator::Configuration.new(File.expand_path('../configuration/navigator.ini', __FILE__))
+  
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
