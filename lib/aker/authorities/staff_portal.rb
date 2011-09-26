f6a3b8d68ee003a354dbc649e61ff8a50acdc016
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-09-26 16:12:10 -0500

    Added initial staff portal authority. #1573.

---
+++ lib/aker/authorities/staff_portal.rb
@@ -0,0 +1,58 @@
+require 'aker'
+
+module Aker::Authorities
+  class StaffPortal
+    
+    def initialize()
+      @groups = build_groups
+      @portal = "NCSNavigator".to_sym
+    end
+
+    def amplify!(user)
+      base = user(user.username)
+      return user unless base
+      user.merge!(base)
+    end
+   
+    def user(username)
+      u = Aker::User.new(username)
+      staff = Staff.find_by_username(username) 
+      u.portals << @portal
+      
+      if staff 
+        attributes = ["first_name", "last_name", "email"]
+        attributes.each do |a|
+          setter = "#{a}="
+          if u.respond_to?(setter)
+            u.send(setter, staff[a])
+          end
+        end
+        u.group_memberships(@portal).concat(load_group_memberships(staff.roles))
+      end
+      u
+    end
+
+    private
+
+    def build_groups
+      groups = []
+      Role.all.each do |role|
+        groups << Aker::Group.new(role.name)
+      end
+      groups
+    end
+
+    def load_group_memberships(roles)
+      gms = []
+      roles.each do |role|
+       gm = Aker::GroupMembership.new(find_group(role.name))
+       gms << gm
+      end
+      gms
+    end
+
+    def find_group(group_name)
+      existing = @groups.find_all { |g| g.name == group_name}.compact.first
+    end
+  end
+end
---
+++ spec/lib/aker/authorities/staff_portal_spec.rb
@@ -0,0 +1,64 @@
+require File.expand_path("../../../../spec_helper", __FILE__)
+
+describe StaffPortal do
+  before do
+    @staff  = FactoryGirl.create(:staff, :username => "lee", :first_name => "Lee", :last_name => "Scott", :email => "scott@test.com")
+    @role = Factory(:role, :name => "Outreach staff")
+    FactoryGirl.create(:staff_role, :role => @role, :staff => @staff)
+    @staff_portal = Aker::Authorities::StaffPortal.new
+  end
+  
+  describe "user" do
+    before do
+      @return_user = @staff_portal.user("lee");
+    end
+    
+    it "copies first name from staff portal user" do
+      @return_user.first_name.should == @staff.first_name
+    end
+    
+    it "copies last name from staff portal user" do
+      @return_user.last_name.should == @staff.last_name
+    end
+    
+    it "copies email from staff portal user" do
+      @return_user.email.should == @staff.email
+    end
+    
+    it "generate group membership from staff role" do
+      @return_user.group_memberships(:NCSNavigator).include?(@role.name).should be_true
+    end
+  end
+
+  describe "#amplify!" do
+    before do
+      @staff_portal.user("lee")
+  
+      @before_lee = Aker::User.new("lee")
+    end
+  
+    def actual
+      @staff_portal.amplify!(@before_lee)
+    end
+  
+    it "does nothing for an unknown user" do
+      lambda { @staff_portal.amplify!(Aker::User.new("lees")) }.should_not raise_error
+    end
+    
+    it "copies simple attributes" do
+      actual.first_name.should == "Lee"
+    end
+
+    it "copies portal" do
+      actual.portals.should == [:NCSNavigator]
+    end
+
+    it "copies group memberships" do
+      actual.group_memberships(:NCSNavigator).size.should == 1
+    end
+    
+    it "contains correct group" do
+      actual.group_memberships(:NCSNavigator).include?("Outreach Staff").should be_true
+    end
+  end
+end
