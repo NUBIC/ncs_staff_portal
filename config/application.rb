4d3b00c5554a096f26ef231db593c5085ba2a3fb
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-09-28 14:23:07 -0500

    Used staff portal authority instead of static for authorization. #1573.

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module OMA
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -51,7 +51,8 @@ module OMA
       # api_mode :http_basic
 
       # The portal to which this application belongs.  Optional.
-      portal :StaffPortal
+      portal :NCSNavigator
+      # portal :StaffPortal
     end
   end
 end
