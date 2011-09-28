8c6ad5a4af686284bc692f23d22be2b1372c469f
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-09-28 15:31:22 -0500

    Added role constants. #1573.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,13 @@
 class Role < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
+  
+  SYSTEM_ADMINISTRATOR = "System Administrator"
+  USER_ADMINISTRATOR = "User Administrator"
+  STAFF_SUPERVISOR = "Staff Supervisor"
+  FIELD_STAFF = "Field Staff"
+  PHONE_STAFF = "Phone Staff"
+  OUTREACH_STAFF = "Outreach Staff"
+  BIOLOGICAL_SPECIMEN_COLLECTOR = "Biological Specimen Collector"
+  SPECIMEN_PROCESSOR = "Specimen Processor"
+  DATA_MANAGER = "Data Manager"
 end
