30f3e07cd0a4e826ff1f1b80ba1d44a5a801f96b
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-11-08 14:21:03 -0600

    Rollback changes.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 job_type :rake_default, "cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
 every :friday, :at => '5pm' do 
-  rake_default 'email:task_reminder', :output => '/var/www/apps/ncs_staff_portal/shared/log/cron.log' if Rails.env.production?
+  rake_default 'email:task_reminder', :output => '/var/www/apps/ncs_staff_portal/shared/log/cron.log'
 end
