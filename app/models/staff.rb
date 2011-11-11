4ce73a4fd966ca0b94b5ee922c8152761307b278
Rhett Sutphin <rhett@detailedbalance.net>
2011-11-11 14:13:06 -0600

    Whitespace.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -31,7 +31,7 @@
 class Staff < ActiveRecord::Base
   self.include_root_in_json = false
   attr_accessor :validate_update, :validate_create
-  
+
   validates_presence_of :first_name, :last_name, :username, :study_center, :if => :create_presence_required?
   validates_presence_of :staff_type, :birth_date, :gender, :race, :ethnicity, :zipcode, :subcontractor, :experience, :pay_type, :if => :update_presence_required?, :on => :update
   validates_uniqueness_of :username
@@ -41,7 +41,7 @@ class Staff < ActiveRecord::Base
   validates_with OtherEntryValidator, :entry => :race, :other_entry => :race_other
   validates_date :ncs_inactive_date, :allow_blank => true
   validate :pay_amount_required, :if => :update_presence_required?, :on => :update
-  
+
   has_many :staff_languages, :dependent => :destroy
   has_many :staff_cert_trainings, :dependent => :destroy
   has_many :staff_weekly_expenses, :dependent => :destroy
@@ -51,33 +51,33 @@ class Staff < ActiveRecord::Base
   has_many :roles, :through => :staff_roles
   has_many :supervisor_employees, :foreign_key => :supervisor_id, :dependent => :destroy
   has_many :employees, :through => :supervisor_employees, :class_name => "Staff"
-  
+
   accepts_nested_attributes_for :supervisor_employees, :allow_destroy => true
   accepts_nested_attributes_for :staff_roles, :allow_destroy => true
   accepts_nested_attributes_for :staff_languages, :allow_destroy => true
-  
+
   before_save :calculate_hourly_rate, :update_employees
-  
+
   def pay_amount_required
     if self.pay_type == "Hourly" or self.pay_type =="Yearly"
       errors.add(:pay_amount, "can't be blank") if self.pay_amount.blank?
       errors.add(:pay_amount, "must be greater than 0") if !self.pay_amount.blank? and self.pay_amount <= 0
     end
   end
-  
+
   def update_employees
     self.employees.delete_all unless self.has_role(Role::STAFF_SUPERVISOR)
   end
 
-  scope :with_role, lambda { |role| 
+  scope :with_role, lambda { |role|
     joins(:roles).where('roles.name = ?', role)
   }
-  
+
   scope :default_supervisors, lambda {
     joins(:roles).
     where("roles.name = 'Staff Supervisor' AND staff.id NOT IN (select supervisor_id from supervisor_employees)")
   }
-  
+
   def as_json(options={})
     json = {}
     json["staff_type"] = self.staff_type ? self.staff_type.display_text : nil
@@ -87,55 +87,58 @@ class Staff < ActiveRecord::Base
     json["ethnicity"] =  self.ethnicity ? self.ethnicity.display_text : nil
     json["experience"] =  self.experience ? self.experience.display_text : nil
     json["languages"] = self.staff_languages.as_json
-    super( :except => [ :id, :pay_type, :pay_amount, :hourly_rate, :created_at, :updated_at, :comment, 
-                             :staff_type_code, :age_range_code, :gender_code,:race_code, :subcontractor_code, 
-                             :ethnicity_code, :experience_code, :birth_date ], 
-           :include =>[:roles]).merge(json)
+    super(
+      :except => [
+        :id, :pay_type, :pay_amount, :hourly_rate, :created_at, :updated_at, :comment,
+        :staff_type_code, :age_range_code, :gender_code,:race_code, :subcontractor_code,
+        :ethnicity_code, :experience_code, :birth_date
+      ],
+      :include =>[:roles]).merge(json)
   end
-  
+
   def expenses_without_pay
-     self.staff_weekly_expenses.where("rate = 0.00 or rate IS NULL")
-   end
-  
+    self.staff_weekly_expenses.where("rate = 0.00 or rate IS NULL")
+  end
+
   def supervisors
     Staff.where("staff.id IN (select supervisor_id from supervisor_employees where employee_id = ?)", self.id)
   end
-  
+
   def visible_employees
     visible_employees = self.employees
     visible_employees = Staff.all if visible_employees.empty? && has_role(Role::STAFF_SUPERVISOR)
     visible_employees
   end
-  
+
   def belongs_to_management_group
-    Role.management_group.any? { |role| self.has_role(role) }  
+    Role.management_group.any? { |role| self.has_role(role) }
   end
-  
+
   def belongs_to_data_collection_group
-    Role.data_collection_group.any? { |role| self.has_role(role) } 
+    Role.data_collection_group.any? { |role| self.has_role(role) }
   end
-  
+
   def has_role(role_name)
     self.roles.map(&:name).include?(role_name)
   end
-  
+
   def is_active
     (ncs_inactive_date.nil?) || (ncs_inactive_date != nil && ncs_inactive_date >= Time.now.to_date) ? true : false
   end
-  
-  
+
+
   def update_presence_required?
     validate_update == "false" ? false : true
   end
-  
+
   def create_presence_required?
     validate_create == "false" ? false : true
   end
-  
-  def name  
-      [first_name, last_name].join(' ')  
+
+  def name
+    [first_name, last_name].join(' ')
   end
-  
+
   def calculate_hourly_rate
     if pay_type == "Hourly"
       self.hourly_rate = pay_amount unless pay_amount.blank?
@@ -146,7 +149,7 @@ class Staff < ActiveRecord::Base
       self.pay_amount = 0
     end
   end
-  
+
   def formatted_birth_date
     birth_date.nil? ? nil : birth_date.to_s
   end
@@ -154,7 +157,7 @@ class Staff < ActiveRecord::Base
   def formatted_birth_date=(birth_date)
     self.birth_date = birth_date
   end
-  
+
   def formatted_ncs_inactive_date
     ncs_inactive_date.nil? ? nil : ncs_inactive_date.to_s
   end
@@ -162,18 +165,18 @@ class Staff < ActiveRecord::Base
   def formatted_ncs_inactive_date=(ncs_inactive_date)
     self.ncs_inactive_date = ncs_inactive_date
   end
-  
+
   def self.by_task_reminder(by_date)
     reminder_staff = []
-    Staff.all.each do |s| 
+    Staff.all.each do |s|
       if (s.staff_weekly_expenses.blank? || !s.staff_weekly_expenses.detect {|expense| expense.week_start_date == by_date.monday})
         reminder_staff << s
       end
     end
     reminder_staff
   end
-  
-  ATTRIBUTE_MAPPING = { 
+
+  ATTRIBUTE_MAPPING = {
     :staff_type_code => "STUDY_STAFF_TYPE_CL1",
     :age_range_code => "AGE_RANGE_CL1",
     :gender_code => "GENDER_CL1",
@@ -182,8 +185,8 @@ class Staff < ActiveRecord::Base
     :ethnicity_code => "ETHNICITY_CL1",
     :experience_code => "EXPERIENCE_LEVEL_CL1",
     :lang_code => "LANGUAGE_CL2",
-    }
-    
+  }
+
   ATTRIBUTE_MAPPING.each do |key, value|
     rel_name = key.to_s.gsub('_code', '')
     belongs_to rel_name, :conditions => "list_name = '#{value}'", :class_name => 'NcsCode', :primary_key => :local_code, :foreign_key => key
--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -34,12 +34,12 @@ describe Staff do
   before(:each) do
     @user_admin =  Role.find_by_name(Role::USER_ADMINISTRATOR)
     @user_admin = FactoryGirl.create(:role, :name => "User Administrator") unless @user_admin
-    
+
     @staff_supervisor = Role.find_by_name(Role::STAFF_SUPERVISOR)
-    @staff_supervisor  = FactoryGirl.create(:role, :name => Role::STAFF_SUPERVISOR) unless @staff_supervisor 
+    @staff_supervisor  = FactoryGirl.create(:role, :name => Role::STAFF_SUPERVISOR) unless @staff_supervisor
   end
-  
-  
+
+
   describe "calculate_hourly_rate" do
     it "should put hourly_rate as it is as pay_amount if pay_type is 'Hourly'" do
       staff= FactoryGirl.build(:staff)
@@ -48,7 +48,7 @@ describe Staff do
       staff.save
       staff.hourly_rate.should == 25.00
     end
-    
+
     it "should put hourly_rate as 'amount/no.of hours worked in year' if pay_type is 'Yearly'" do
       staff= FactoryGirl.build(:staff)
       staff.pay_type = "Yearly"
@@ -56,7 +56,7 @@ describe Staff do
       staff.save
       staff.hourly_rate.should == 28.57
     end
-    
+
     it "should set hourly_rate as 0 and pay_amount as 0 if pay_type is 'Voluntary'" do
       staff= FactoryGirl.build(:staff)
       staff.pay_type = "Voluntary"
@@ -64,36 +64,36 @@ describe Staff do
       staff.hourly_rate.should == 0
       staff.pay_amount.should == 0
     end
-    
+
     it "should not calculate the hourly_rate if pay_amount is blank && pay_type is 'Yearly'" do
       staff= FactoryGirl.build(:staff)
       staff.pay_type = "Yearly"
       staff.save
       staff.hourly_rate.should == nil
     end
-    
+
     it "should not calculate the hourly_rate if pay_amount is blank && pay_type is 'Hourly'" do
       staff= FactoryGirl.build(:staff)
       staff.pay_type = "Hourly"
       staff.save
       staff.hourly_rate.should == nil
     end
-    
+
   end
-  
+
   it { should have_many(:staff_roles) }
   it { should have_many(:roles).through(:staff_roles) }
-  
+
   it { should have_many(:supervisor_employees) }
   it { should have_many(:employees).through(:supervisor_employees) }
-  
+
   describe "weekly_task_reminder" do
     before(:each) do
       @staff1 = FactoryGirl.create(:staff)
       @staff2 = FactoryGirl.create(:staff)
       @staff3 = FactoryGirl.create(:staff)
       @staff4 = FactoryGirl.create(:staff)
-    
+
       @staff3.staff_weekly_expenses.create(:week_start_date => Date.today.monday)
       @staff2.staff_weekly_expenses.create(:week_start_date => (Date.today - 1.week).monday)
       @staff1.staff_weekly_expenses.create(:week_start_date => (Date.today - 1.week).monday)
@@ -104,16 +104,16 @@ describe Staff do
       expected_staff.should include @staff2
       expected_staff.should include @staff4
     end
-    
+
     it "should return all the staff with no weekly task entry for the previous week" do
       expected_staff = Staff.by_task_reminder(Date.today - 1.week)
       expected_staff.should include @staff3
       expected_staff.should include @staff4
     end
   end
-  
+
   describe "validates" do
-    
+
     describe "staff_type" do
       let(:staff_type_code) { Factory(:ncs_code, :list_name => "STUDY_STAFF_TYPE_CL1", :display_text => "Other", :local_code => -5) }
 
@@ -138,7 +138,7 @@ describe Staff do
         staff.staff_type_other.should == nil
       end
     end
-    
+
     describe "race" do
       let(:race_code) { Factory(:ncs_code, :list_name => "RACE_CL1", :display_text => "Other", :local_code => -5) }
 
@@ -163,7 +163,7 @@ describe Staff do
         staff.race_other.should == nil
       end
     end
-    
+
     describe "presence" do
       it "first_name, last_name, email and studycenter is required if validate_create is not set" do
         staff = FactoryGirl.build(:staff, :username => "test123", :first_name => nil, :last_name => nil, :email => nil , :study_center => nil)
@@ -172,7 +172,7 @@ describe Staff do
         staff.should have(1).error_on(:last_name)
         staff.should have(1).error_on(:study_center)
       end
-      
+
       it "staff_type, birth_date, gender, race, ethnicity, zipcode, subcontractor, experience is required if validate_update is not set" do
         staff = FactoryGirl.create(:staff)
         staff.save
@@ -186,14 +186,14 @@ describe Staff do
         staff.should have(1).error_on(:subcontractor)
         staff.should have(1).error_on(:experience)
       end
-      
+
       it "staff_type, birth_date, gender, race, ethnicity, zipcode, subcontractor, experience is not required if validate_update is false" do
         staff = FactoryGirl.create(:staff, :validate_update => "false")
         staff.save
         staff.should be_valid
       end
     end
-    
+
     describe "pay_amount_required" do
       before(:each) do
         @staff = FactoryGirl.create(:staff)
@@ -207,13 +207,13 @@ describe Staff do
         @staff.birth_date = Date.today - 18.year
         @staff.pay_type = "Hourly"
       end
-      
+
       it "throws error if pay_amount is blank" do
         @staff.should_not be_valid
         @staff.should have(1).error_on(:pay_amount)
         @staff.errors[:pay_amount].should == ["can't be blank"]
       end
-      
+
       it "pay_amount should not contain other than decimal value" do
         @staff.pay_amount = "invalid"
         @staff.save
@@ -231,55 +231,55 @@ describe Staff do
       end
     end
   end
-  
+
   describe "has_role" do
     before(:each) do
       @staff = FactoryGirl.create(:staff)
       @staff.roles << @user_admin
     end
-    
+
     it "returns true if staff has particuler role" do
       @staff.has_role("User Administrator").should == true
     end
-    
+
     it "returns false if staff has particuler role" do
       @staff.has_role("System Administrator").should == false
     end
   end
-  
+
   describe "is_active" do
     before(:each) do
       @staff = FactoryGirl.create(:staff)
     end
-    
+
     it "returns true if staff has ncs_inactive_date as nil" do
       @staff.is_active.should == true
     end
-    
+
     it "returns true if staff has ncs_inactive_date as of today" do
       @staff.ncs_inactive_date = Time.now.to_date
       @staff.is_active.should == true
     end
-    
+
     it "returns true if staff has ncs_inactive_date greater than today" do
       @staff.ncs_inactive_date = Time.now.to_date + 7.day
       @staff.is_active.should == true
     end
-    
+
     it "returns false if staff has ncs_inactive_date less then today's date" do
       @staff.ncs_inactive_date = Time.now.to_date - 7.day
       @staff.is_active.should == false
     end
   end
-  
+
   describe "supervisor employees" do
     before(:each) do
       @sup = FactoryGirl.create(:staff, :first_name => "Super Supervisor")
       @sup.roles << @staff_supervisor
-      
+
       @sup1 = FactoryGirl.create(:staff, :first_name => "Supervisor")
       @sup1.roles << @staff_supervisor
-      
+
       @staff1 = FactoryGirl.create(:staff)
       @staff2 = FactoryGirl.create(:staff)
       @staff3 = FactoryGirl.create(:staff)
@@ -290,13 +290,13 @@ describe Staff do
         actual_sup = Staff.default_supervisors
         actual_sup.should include @sup
       end
-    
+
       it "does not include the supervisor who has employee assigned" do
         actual_sup = Staff.default_supervisors
         actual_sup.should_not include @sup1
       end
     end
-    
+
     describe "visible_employees" do
       it "returns all the assigned employees for supervisor" do
         employees = @sup1.visible_employees
@@ -304,177 +304,177 @@ describe Staff do
         employees.should include @staff2
         employees.should_not include @staff3
       end
-      
+
       it "returns all the employees if there is no assigned employee for supervisor" do
         employees = @sup.visible_employees
         employees.should include @staff1
         employees.should include @staff2
         employees.should include @staff3
       end
-      
+
       it "returns empty array for staff other then supervisor" do
         other = FactoryGirl.create(:staff)
         employees = other.visible_employees
         employees.should be_empty
       end
     end
-    
+
     describe "update_employees" do
       it "does nothing if staff has supervisor role" do
         @sup1.employees.should include @staff1
         @sup1.employees.should include @staff2
       end
-      
+
       it "delete all the employees if staff doesn't have Staff Supervisor Role" do
         @sup1.validate_update = "false"
-        @sup1.roles.delete_all 
+        @sup1.roles.delete_all
         @sup1.save
         @sup1.employees.should be_empty
       end
     end
-    
+
     describe "supervisors" do
       it "returns assigned supervisors" do
         supervisors = @staff1.supervisors
         supervisors.count.should == 1
         supervisors.should include @sup1
       end
-      
+
       it "should not return un assigned supervisors" do
         supervisors = @staff1.supervisors
         supervisors.should_not include @sup
       end
     end
   end
- 
+
   describe "as_json" do
     before(:each) do
       @staff = FactoryGirl.create(:staff)
     end
-    
+
     ["username", "first_name", "last_name", "email", "study_center", "ncs_active_date", "ncs_inactive_date", "staff_type_other", "race_other"].each do |key|
       it "contains #{key}" do
         @staff.as_json.has_key?("#{key}").should == true
       end
     end
-    
+
     ["pay_type", "pay_amount", "birth_date", "hourly_rate", "created_at", "updated_at", "comment", "staff_type_code", "age_range_code", "gender_code", "subcontractor_code", "ethnicity_code", "experience_code"].each do |key|
       it "does not contain #{key}" do
         @staff.as_json.has_key?("#{key}").should == false
       end
     end
-    
+
     describe "contains staff_type" do
       it "key" do
         @staff.as_json.has_key?("staff_type").should == true
       end
-      
+
       it "value to nil" do
         @staff.as_json["staff_type"].should == nil
       end
-      
+
       it "value to actual display name" do
-        Factory.create(:ncs_code, :list_name => "STUDY_STAFF_TYPE_CL1", :display_text => "Principal Investigator", :local_code => 1) 
+        Factory.create(:ncs_code, :list_name => "STUDY_STAFF_TYPE_CL1", :display_text => "Principal Investigator", :local_code => 1)
         @staff.staff_type_code = 1
         actual_json = @staff.as_json
         actual_json["staff_type"].should == "Principal Investigator"
       end
     end
-    
+
     describe "contains gender" do
       it "key" do
         @staff.as_json.has_key?("gender").should == true
       end
-      
+
       it "value to nil" do
         @staff.as_json["gender"].should == nil
       end
-      
+
       it "value to actual display name" do
-        Factory.create(:ncs_code, :list_name => "GENDER_CL1", :display_text => "Male", :local_code => 1) 
+        Factory.create(:ncs_code, :list_name => "GENDER_CL1", :display_text => "Male", :local_code => 1)
         @staff.gender_code = 1
         actual_json = @staff.as_json
         actual_json["gender"].should == "Male"
       end
     end
-    
+
     describe "contains subcontractor" do
       it "key" do
         @staff.as_json.has_key?("subcontractor").should == true
       end
-      
+
       it "value to nil" do
         @staff.as_json["subcontractor"].should == nil
       end
-      
+
       it "value to actual display name" do
-        Factory.create(:ncs_code, :list_name => "CONFIRM_TYPE_CL2", :display_text => "Yes", :local_code => 1) 
+        Factory.create(:ncs_code, :list_name => "CONFIRM_TYPE_CL2", :display_text => "Yes", :local_code => 1)
         @staff.subcontractor_code = 1
         actual_json = @staff.as_json
         actual_json["subcontractor"].should == "Yes"
       end
     end
-    
+
     describe "contains race" do
       it "key" do
         @staff.as_json.has_key?("race").should == true
       end
-      
+
       it "value to nil" do
         @staff.as_json["race"].should == nil
       end
-      
+
       it "value to actual display name" do
-        Factory.create(:ncs_code, :list_name => "RACE_CL1", :display_text => "White", :local_code => 1) 
+        Factory.create(:ncs_code, :list_name => "RACE_CL1", :display_text => "White", :local_code => 1)
         @staff.race_code = 1
         actual_json = @staff.as_json
         actual_json["race"].should == "White"
       end
     end
-    
+
     describe "contains ethinicity" do
       it "key" do
         @staff.as_json.has_key?("ethnicity").should == true
       end
-      
+
       it "value to nil" do
         @staff.as_json["ethnicity"].should == nil
       end
-      
+
       it "value to actual display name" do
-        Factory.create(:ncs_code, :list_name => "ETHNICITY_CL1", :display_text => "Hispanic or Latino", :local_code => 1) 
+        Factory.create(:ncs_code, :list_name => "ETHNICITY_CL1", :display_text => "Hispanic or Latino", :local_code => 1)
         @staff.ethnicity_code = 1
         actual_json = @staff.as_json
         actual_json["ethnicity"].should == "Hispanic or Latino"
       end
     end
-    
+
     describe "contains experience" do
       it "key" do
         @staff.as_json.has_key?("experience").should == true
       end
-      
+
       it "value to nil" do
         @staff.as_json["experience"].should == nil
       end
-      
+
       it "value to actual display name" do
-        Factory.create(:ncs_code, :list_name => "EXPERIENCE_LEVEL_CL1", :display_text => "Less than 1 year", :local_code => 1) 
+        Factory.create(:ncs_code, :list_name => "EXPERIENCE_LEVEL_CL1", :display_text => "Less than 1 year", :local_code => 1)
         @staff.experience_code = 1
         actual_json = @staff.as_json
         actual_json["experience"].should == "Less than 1 year"
       end
     end
-    
+
     describe "contains roles" do
       it "key" do
         @staff.as_json.has_key?(:roles).should == true
       end
-      
+
       it "should contain empty array if no roles assigned" do
         @staff.as_json[:roles].count.should == 0
       end
-      
+
       it "should contain roles name for assigned roles" do
         @staff.roles << @user_admin
         @staff.roles << @staff_supervisor
@@ -484,16 +484,16 @@ describe Staff do
         json_roles[1]["name"].should == "Staff Supervisor"
       end
     end
-    
+
     describe "contains languages" do
       it "key" do
         @staff.as_json.has_key?("languages").should == true
       end
-      
+
       it "should contain empty array if no languages assigned" do
         @staff.as_json["languages"].count.should == 0
       end
-      
+
       it "should contain languages name for assigned roles" do
         @staff.staff_languages << FactoryGirl.create(:staff_language)
         json_languages = @staff.as_json["languages"]
@@ -502,19 +502,19 @@ describe Staff do
       end
     end
   end
-  
+
   describe "expenses_without_pay" do
     before(:each) do
       @staff = FactoryGirl.create(:staff)
       @staff.staff_weekly_expenses.create(:week_start_date => Date.today.monday, :rate => 22.56)
     end
-    
+
     it "contain expenses with null rate" do
       @staff.staff_weekly_expenses.create(:week_start_date => (Date.today - 1.week).monday, :rate => nil)
       expenses = @staff.expenses_without_pay
       expenses.count.should == 1
     end
-    
+
     it "contain expenses with 0.00 rate" do
       @staff.staff_weekly_expenses.create(:week_start_date => (Date.today - 1.week).monday, :rate => nil)
       @staff.staff_weekly_expenses.create(:week_start_date => (Date.today - 2.week).monday, :rate => 0.00)
@@ -522,7 +522,7 @@ describe Staff do
       expenses.count.should == 2
     end
   end
-  
+
   describe "belongs_to" do
     before(:each) do
       @staff = FactoryGirl.create(:staff)
@@ -534,27 +534,27 @@ describe Staff do
         @staff.roles << @user_admin
         @staff.belongs_to_management_group.should == true
       end
-      
+
       it "returns false if staff has no role" do
         @staff.belongs_to_management_group.should == false
       end
-      
+
       it "returns false if staff has no any role from management_group" do
         @staff.roles << @data_collection_group
         @staff.belongs_to_management_group.should == false
       end
     end
-    
+
     describe "data_collection_group" do
       it "returns true if staff has any role from data_collection_group" do
         @staff.roles << @data_collection_group
         @staff.belongs_to_data_collection_group.should == true
       end
-      
+
       it "returns false if staff has no role" do
         @staff.belongs_to_data_collection_group.should == false
       end
-      
+
       it "returns false if staff has no any role from data_collection_group" do
         @staff.roles << @user_admin
         @staff.belongs_to_data_collection_group.should == false
