818e178057b775af38762c317a6c01afe6ea4307
Jalpa Patel <jalpapatel@dhcp-165-124-223-188.nubic.northwestern.edu>
2012-05-31 11:37:04 -0500

    Added NcsCode lookups in mdes_record. #1598.

--- lib/mdes_record.rb
+++ lib/mdes_record.rb
@@ -32,6 +32,29 @@ module MdesRecord
 
     module ClassMethods
       attr_accessor :public_id_attribute_name
+      attr_accessor :attribute_names
+    end
+    
+    class NcsCodedAttribute
+      attr_reader :attribute_name, :list_name
+
+      def initialize(model_class, attribute_name, list_name)
+        @list_name = list_name.upcase
+        @attribute_name = attribute_name.to_sym
+        belongs_to!(model_class)
+      end
+
+      def foreign_key
+        @foreign_key ||= "#{attribute_name}_code".to_sym
+      end
+
+      def belongs_to!(model)
+        model.belongs_to(attribute_name,
+          :conditions => "list_name = '#{list_name}'",
+          :foreign_key => foreign_key,
+          :class_name => 'NcsCode',
+          :primary_key => :local_code)
+      end
     end
 
     module InstanceMethods
@@ -46,17 +69,33 @@ module MdesRecord
       def public_id=(value)
         self.send("#{self.class.public_id_attribute_name}=", value)
       end
+      
+      def get_attribute_names
+        (self.attribute_names - ["id"]).sort
+      end
+      private :get_attribute_names
 
       def generate_public_id
         self.public_id = ActsAsMdesRecord.create_public_id_string
       end
       private :generate_public_id
+      
+
     end
   end
 
   def acts_as_mdes_record(options)
     self.send(:include, ActsAsMdesRecord)
     self.public_id_attribute_name = options[:public_id]
+  end
+  
+  def ncs_coded_attribute(attribute_name, list_name)
+    ncs_coded_attributes[attribute_name.to_sym] =
+      ::MdesRecord::ActsAsMdesRecord::NcsCodedAttribute.new(self, attribute_name, list_name)
+  end
+
+  def ncs_coded_attributes
+    @ncs_coded_attributes ||= {}
   end
 end
 
