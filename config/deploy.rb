be19f7d60d2a1d11c66adc62e153f95e61c0c085
David Yip <yipdw@northwestern.edu>
2012-06-12 15:51:51 -0500

    Remove PATH munging: it's not needed anymore.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ ssh_options[:forward_agent] = true
 
 # Version control
 default_run_options[:pty]   = true # to get the passphrase prompt from git
-default_environment['PATH'] = '/opt/ruby-enterprise/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin'
 
 set :scm, "git"
 set :repository, bcconf["repo"]
