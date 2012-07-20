4746712c0d4a555c3dfadca72f9a6d02cddff3a2
Jalpa Patel <jalpapatel@dhcp-165-124-223-245.nubic.northwestern.edu>
2012-07-20 08:57:54 -0500

    Added pagination to the all staff_cert_trainings if save is not successful. Fixes #2300.

--- app/controllers/staff_cert_trainings_controller.rb
+++ app/controllers/staff_cert_trainings_controller.rb
@@ -31,13 +31,12 @@ class StaffCertTrainingsController < SecuredController
   def create
     @staff = Staff.find(params[:staff_id])
     @staff_cert_training = @staff.staff_cert_trainings.build(params[:staff_cert_training])
-
     respond_to do |format|
       if @staff_cert_training.save
         format.html { redirect_to(new_staff_staff_cert_training_path(@staff), :notice => 'Staff cert training was successfully created.') }
         format.xml  { render :xml => @staff_cert_training, :status => :created, :location => @staff_cert_training }
       else
-        @staff_cert_trainings = Staff.find(params[:staff_id]).staff_cert_trainings
+        @staff_cert_trainings = @staff.staff_cert_trainings.paginate(:page => params[:page], :per_page => 20)
         format.html { render :action => "new", :staff_cert_trainings => @staff_cert_trainings }
         format.xml  { render :xml => @staff_cert_training.errors, :status => :unprocessable_entity }
       end
@@ -49,13 +48,12 @@ class StaffCertTrainingsController < SecuredController
   def update
     @staff = Staff.find(params[:staff_id])
     @staff_cert_training = @staff.staff_cert_trainings.find(params[:id])
-
     respond_to do |format|
       if @staff_cert_training.update_attributes(params[:staff_cert_training])
         format.html { redirect_to(new_staff_staff_cert_training_path(@staff), :notice => 'Staff cert training was successfully updated.') }
         format.xml  { head :ok }
       else
-        @staff_cert_trainings = @staff.staff_cert_trainings
+        @staff_cert_trainings = @staff.staff_cert_trainings.paginate(:page => params[:page], :per_page => 20)
         format.html { render :action => "edit" }
         format.xml  { render :xml => @staff_cert_training.errors, :status => :unprocessable_entity }
       end
