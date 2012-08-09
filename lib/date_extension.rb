cae241437f0594b3f6c48d0df13527c4de349bda
Jalpa Patel <jalpapatel@dhcp-165-124-223-184.nubic.northwestern.edu>
2012-08-09 09:38:18 -0500

    Display the datetime into date. #2356.

--- lib/date_extension.rb
+++ lib/date_extension.rb
@@ -1,10 +1,10 @@
 class Date
   def beginning_of_week
     if StaffPortal.week_start_day == "monday"
-  	  days_to_monday = wday!=0 ? wday-1 : 6
-  	  (self - days_to_monday).midnight
+  	  days_to_start_day = wday!=0 ? wday-1 : 6
   	else
-  	  (self - self.wday.days).midnight
+  	  days_to_start_day = wday.days
   	end
+    (self - days_to_start_day).midnight.strftime('%Y-%m-%d') 
   end
 end
