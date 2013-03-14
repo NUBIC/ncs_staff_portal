dbd68872b12d8a30ef97ebdd746b11a57881ae4d
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-03-14 11:43:17 -0500

    Restructure Importer to allow different versions of MDES. Add Importer for MDES 2.0. #2574.

--- lib/ncs_navigator/staff_portal/warehouse/two_point_zero.rb
+++ lib/ncs_navigator/staff_portal/warehouse/two_point_zero.rb
@@ -2,5 +2,6 @@ require 'ncs_navigator/staff_portal'
 module NcsNavigator::StaffPortal::Warehouse
   module TwoPointZero
     autoload :Enumerator, 'ncs_navigator/staff_portal/warehouse/two_point_zero/enumerator'
+    autoload :Importer, 'ncs_navigator/staff_portal/warehouse/two_point_zero/importer'
   end
 end
