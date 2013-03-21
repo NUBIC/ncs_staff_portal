c34f54ff68a8d5c78c39fc97158b979555b5285e
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-03-21 16:35:38 -0500

    Add enumerator for MDES 3.2. Closes #3629.

---
+++ lib/ncs_navigator/staff_portal/warehouse/three_point_two.rb
@@ -0,0 +1,6 @@
+require 'ncs_navigator/staff_portal'
+module NcsNavigator::StaffPortal::Warehouse
+  module ThreePointTwo
+    autoload :Enumerator, 'ncs_navigator/staff_portal/warehouse/three_point_two/enumerator'
+  end
+end
--- spec/lib/ncs_navigator/staff_portal/warehouse/enumerator_spec.rb
+++ spec/lib/ncs_navigator/staff_portal/warehouse/enumerator_spec.rb
@@ -50,6 +50,16 @@ module NcsNavigator::StaffPortal::Warehouse
         end
       end
 
+      describe 'for MDES 3.2' do
+        before do
+          wh_config.mdes_version = '3.2'
+        end
+
+        it 'uses the TwoPointOne version' do
+          actual.enum.should be_a ThreePointTwo::Enumerator
+        end
+      end
+
       # N.b.: this test is only actually possible if there is a version
       # of the MDES that the Warehouse supports but for which there is no
       # corresponding op enumerator. This is true now but will not always be.
