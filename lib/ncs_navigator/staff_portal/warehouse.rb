13c04dc550c73c950e099dd0879e1bd3c561bd20
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-04-19 16:42:19 -0500

    Add MDES 3.1 Enumerator.

--- lib/ncs_navigator/staff_portal/warehouse.rb
+++ lib/ncs_navigator/staff_portal/warehouse.rb
@@ -11,6 +11,7 @@ module NcsNavigator::StaffPortal
     autoload :TwoPointOne,                  'ncs_navigator/staff_portal/warehouse/two_point_one'
     autoload :TwoPointTwo,                  'ncs_navigator/staff_portal/warehouse/two_point_two'
     autoload :ThreePointZero,               'ncs_navigator/staff_portal/warehouse/three_point_zero'
+    autoload :ThreePointOne,                'ncs_navigator/staff_portal/warehouse/three_point_one'
     autoload :ThreePointTwo,                'ncs_navigator/staff_portal/warehouse/three_point_two'
   end
 end
---
+++ lib/ncs_navigator/staff_portal/warehouse/three_point_one.rb
@@ -0,0 +1,6 @@
+require 'ncs_navigator/staff_portal'
+module NcsNavigator::StaffPortal::Warehouse
+  module ThreePointOne
+    autoload :Enumerator, 'ncs_navigator/staff_portal/warehouse/three_point_one/enumerator'
+  end
+end
